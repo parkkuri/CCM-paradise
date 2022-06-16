745339a25c65f6d663dee27fbdf976bdabd5a2ef
kuri-thevc <60633084+ku-ri@users.noreply.github.com>
2022-06-16 23:33:50 +0900

    fix : static_root 변경

--- ccm_paradise/ccm_paradise/settings.py
+++ ccm_paradise/ccm_paradise/settings.py
@@ -148,6 +148,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
